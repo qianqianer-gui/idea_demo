047c82c9c53b6ffb2f0bd64b1cd915f1322fb872
gui <1215083154@qq.com>
2022-04-19 14:51:01 +0800

    Class.forname(package.class)

--- src/mytest/CalculatorTest.java
+++ src/mytest/CalculatorTest.java
@@ -7,7 +7,7 @@ public class CalculatorTest {
     @Test
     public void shouldReturn2When1Plus1() throws IllegalAccessException, ClassNotFoundException, InstantiationException {
         Calculator calculator = new Calculator();
-        int actualResult = calculator.compute(1,1,"Add");
+        int actualResult = calculator.compute(1,1,"mytest.Add");
         int expectResult = 2;
         Assert.assertEquals(expectResult,actualResult);
     }
@@ -15,7 +15,7 @@ public class CalculatorTest {
     @Test
     public void shouldReturn0When1Sub1() throws IllegalAccessException, ClassNotFoundException, InstantiationException {
         Calculator calculator = new Calculator();
-        int actualResult = calculator.compute(1,1,"Sub");
+        int actualResult = calculator.compute(1,1,"mytest.Sub");
         int expectResult = 0;
         Assert.assertEquals(expectResult,actualResult);
     }
